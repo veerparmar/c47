af34cf6c5a41f54a6d83b9b3088ac1b82492391d
veerparmar <78463128+veerparmar@users.noreply.github.com>
2021-10-20 20:27:26 +1300

    Add files via upload

---
+++ make your own game/sketch.js
@@ -0,0 +1,87 @@
+var zombie, zombie_img
+var player, player_img
+var varFlag = 0
+var zombieGroup
+var score=0;
+
+function preload(){
+  zombie_img = loadImage("images/zombie.png");
+  player_img = loadImage("images/player.png")
+}
+
+function setup() {
+  createCanvas(600,600);
+  player = createSprite(550,550, 200, 200);
+  player.addImage("player",player_img)
+  player.scale = 0.2
+  zombieGroup = createGroup()
+}
+
+function draw() {
+  background(160,100,0);  
+  drawSprites();
+  fill("black")
+  text("Score: "+ score, 500,30);
+  textSize(20);
+  
+  score = score + Math.round(getFrameRate()/60);
+  
+  
+if(zombieGroup.isTouching(player)){
+  zombieGroup.destroyEach()
+  score = 0
+  
+  
+}
+
+if (mousePressedOver(zombie)){
+
+  var x = mouseX
+  var y = mouseY
+  console.log("hit")
+  
+  zombieGroup.destroyEach()
+  
+}  
+
+
+  
+
+spawnZombies();
+}
+
+
+function spawnZombies(){
+
+  if((frameCount %60 === 0) && zombieGroup.length === 0){
+    if(varFlag == 0){
+      xPosition = -5
+      yPosition = Math.round(random(0,150))
+      varFlag = 1
+
+    }
+    else{
+      yPosition = -5
+      xPosition = Math.round(random(0,150))
+      varFlag = 0
+    }
+
+
+    zombie = createSprite(xPosition,yPosition,50,50)
+    zombie.addImage("zombie",zombie_img)
+    zombie.scale = 0.1
+    zombie.velocityX = 3
+    zombie.velocityY = 3
+    
+    zombie.lifetime = 300
+    zombieGroup.add(zombie)
+  }
+
+
+
+}
+
+
+
+
+
